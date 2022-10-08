01200755a1ac8b98fbd40643932522711bcd5932
XOrcistGans <112866322+XOrcistGans@users.noreply.github.com>
2022-10-09 02:00:19 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -20,23 +20,23 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //------------ BIO
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `${htki} *BIODATA* ${htka}
-${htjava} *💌 Nama* : WH-MODS-Dev
-${htjava} *✉️ Nama RL* : HAMBA ALLAH
+${htjava} *💌 Nama* : 𝐗𝐎𝐫𝐜𝐢𝐬𝐭𝐆𝐚𝐧𝐬
+${htjava} *✉️ Nama RL* : 𝐗𝐎𝐗𝐎
 ${htjava} *♂️ Gender* : Boys
 ${htjava} *🕋 Agama* : Islam
-${htjava} *⏰ Tanggal lahir* : Private 🥶
-${htjava} *🎨 Umur* : 18
-${htjava} *🧮 Kelas* : SUDAH TAMAT
-${htjava} *🧩 Hobby* : MODIFIKASI WHATSAPP "MODDER WHATSAPP"
-${htjava} *💬 Sifat* : ASIK DAH KALAU DAH KENAL
-${htjava} *🗺️ Tinggal* : Indo, Depok, Jawa Barat
+${htjava} *⏰ Tanggal lahir* : November, Jan lupa rayain ya 🗿
+${htjava} *🎨 Umur* : Private
+${htjava} *🧮 Kelas* : SMK
+${htjava} *🧩 Hobby* : SadBoy :) "MODDER WHATSAPP"
+${htjava} *💬 Sifat* : Gw Orngnya Pendiem Kok🗿
+${htjava} *🗺️ Tinggal* : Konoha, Kepolu, Jawa Barat
 ${htjava} *❤️ Waifu* : -
 
 ${htjava} *📷 ɪɴsᴛᴀɢʀᴀᴍ* : ${sig}
 ${htjava} *🐈 ɢɪᴛʜᴜʙ:* ${sgh}
 •·––––––––––––––––––––––––––·•
 `
-  let teks = 'Pilih dibawah kak ! o(〃＾▽＾〃)o'
+  let teks = 'Pilih dibawah Beban ! o(〃＾▽＾〃)o'
 const sections = [
    {
 	title: `${htjava} OWNER –––––––––·•`,
